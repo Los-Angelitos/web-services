8d69e29c3e89658df10512e1748c76a0a50c52ba
Mauricio <mauriciorojasanchez@gmail.com>
2025-04-10 22:38:13 -0500

    feat(organizational-management): added aggregates.

---
+++ SweetManagerIotWebService.API/OrganizationalManagement/Domain/Model/Aggregates/Hotel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using SweetManagerIotWebService.API.IAM.Domain.Model.Aggregates;
+using SweetManagerIotWebService.API.Reservations.Domain.Model.Aggregates;
+
+namespace SweetManagerIotWebService.API;
+
+public partial class Hotel
+{
+    public int Id { get; set; }
+
+    public int? OwnerId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? Phone { get; set; }
+
+    public virtual Owner? Owner { get; set; }
+
+    public virtual ICollection<Room> Rooms { get; set; } = new List<Room>();
+
+    public virtual ICollection<Supply> Supplies { get; set; } = new List<Supply>();
+}
---
+++ SweetManagerIotWebService.API/OrganizationalManagement/Domain/Model/Aggregates/Provider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SweetManagerIotWebService.API.OrganizationalManagement.Domain.Model.Aggregates;
+
+public partial class Provider
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? State { get; set; }
+
+    public virtual ICollection<Supply> Supplies { get; set; } = new List<Supply>();
+}
