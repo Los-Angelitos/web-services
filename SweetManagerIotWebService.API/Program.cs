9b336186c74a6642a3335d36f740d12ac3ed1160
Mauricio <mauriciorojasanchez@gmail.com>
2025-04-10 22:50:58 -0500

    chore: configured main port.

--- SweetManagerIotWebService.API/Program.cs
+++ SweetManagerIotWebService.API/Program.cs
@@ -1,18 +1,169 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using MySql.Data.MySqlClient;
+using SweetManagerIotWebService.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using SweetManagerIotWebService.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using System.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+
+// Configure Lowercase URLs
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#region Database Configuration
+// Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString("SweetManager");
+
+builder.Services.AddTransient<IDbConnection>(db => new MySqlConnection(connectionString));
+
+var connectionStringFromEnvironment = Environment.GetEnvironmentVariable("SweetManagerDbConnection");
+
+if (connectionStringFromEnvironment != null)
+{
+    connectionString = connectionStringFromEnvironment;
+}
+
+// Configure Database Context and Logging Levels
+builder.Services.AddDbContext<SweetManagerContext>(
+    options =>
+    {
+        if (connectionString != null)
+            if (builder.Environment.IsDevelopment())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+            else if (builder.Environment.IsProduction())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Error)
+                    .EnableDetailedErrors();
+    });
+
+#endregion
+
+#region OPENAPI Configuration
+// Configure Lowercase URLs
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1",
+            new OpenApiInfo
+            {
+                Title = "Sweet Manager API",
+                Version = "v1",
+                Description = "Sweet Manager API",
+                TermsOfService = new Uri("https://acme-learning.com/tos"),
+                Contact = new OpenApiContact
+                {
+                    Name = "Sweet Manager Studios",
+                    Email = "contact@swm.com"
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "Apache 2.0",
+                    Url = new Uri("https://www.apache.org/licenses/LICENSE-2.0.html")
+                }
+            });
+        c.EnableAnnotations();
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "bearer"
+        });
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Id = "Bearer", Type = ReferenceType.SecurityScheme
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+    });
+
+#endregion
+
+builder.Services.AddHttpContextAccessor();
+
+#region Dependency Injection
+
+// IAM Bounded context
+
+
+// Reservations bounded context
+
+
+// Commerce Bounded context
+
+
+// Inventory Bounded Context
+
+
+// Communication Bounded context
+
+
+
+// Organizational Management Bounded context
+
+
+
+#endregion
+
+#region JWT Configuration
+
+// In progress
+
+#endregion
+
 var app = builder.Build();
+
+#region Ensure Database Created (COMPILE AppDbContext)
+// Verify Database Objects are created
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<SweetManagerContext>();
+    context.Database.EnsureCreated();
+}
+
+#endregion
+
+#region Run DatabaseInitializer
+using (var scope = app.Services.CreateScope())
+{
+    // In progress
+}
+#endregion
+
+// Configuration cors
+app.UseCors(
+    b => b.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()
+);
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
