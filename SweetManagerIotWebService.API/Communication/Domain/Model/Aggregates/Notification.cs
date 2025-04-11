0bc03d5339d2ec126c8ccdaa1e2379a5f7f721f0
Mauricio <mauriciorojasanchez@gmail.com>
2025-04-10 22:35:12 -0500

    feat(communication): added aggregate.

---
+++ SweetManagerIotWebService.API/Communication/Domain/Model/Aggregates/Notification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SweetManagerIotWebService.API.Communication.Domain.Model.Aggregates;
+
+public partial class Notification
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Content { get; set; }
+
+    public string? SenderType { get; set; }
+
+    public int? SenderId { get; set; }
+
+    public int? ReceiverId { get; set; }
+}
