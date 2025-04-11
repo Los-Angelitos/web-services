76405c6ad376722d0aceadbec5ecb1ef27138f35
Mauricio <mauriciorojasanchez@gmail.com>
2025-04-10 22:37:18 -0500

    feat(inventory): added aggregate.

---
+++ SweetManagerIotWebService.API/Inventory/Domain/Model/Aggregates/Supply.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using SweetManagerIotWebService.API.Inventory.Domain.Model.Entities;
+using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Model.Aggregates;
+
+namespace SweetManagerIotWebService.API;
+
+public partial class Supply
+{
+    public int Id { get; set; }
+
+    public int? ProviderId { get; set; }
+
+    public int? HotelId { get; set; }
+
+    public string? Name { get; set; }
+
+    public decimal? Price { get; set; }
+
+    public int? Stock { get; set; }
+
+    public string? State { get; set; }
+
+    public virtual Hotel? Hotel { get; set; }
+
+    public virtual Provider? Provider { get; set; }
+
+    public virtual ICollection<SupplyRequest> SupplyRequests { get; set; } = new List<SupplyRequest>();
+}
