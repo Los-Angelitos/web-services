7caa7ab2e0f75e58a1b6df8d54c79ce890ea991d
Mauricio <mauriciorojasanchez@gmail.com>
2025-04-10 22:38:31 -0500

    feat(reservations): added aggregates.

---
+++ SweetManagerIotWebService.API/Reservations/Domain/Model/Aggregates/Booking.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using SweetManagerIotWebService.API.Commerce.Domain.Model.Aggregates;
+using SweetManagerIotWebService.API.IAM.Domain.Model.Entities;
+using SweetManagerIotWebService.API.Reservations.Domain.Model.Aggregates;
+
+namespace SweetManagerIotWebService.API;
+
+public partial class Booking
+{
+    public int Id { get; set; }
+
+    public int? PaymentCustomerId { get; set; }
+
+    public int? RoomId { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateTime? StartDate { get; set; }
+
+    public DateTime? FinalDate { get; set; }
+
+    public decimal? PriceRoom { get; set; }
+
+    public int? NightCount { get; set; }
+
+    public decimal? Amount { get; set; }
+
+    public string? State { get; set; }
+
+    public int? PreferenceId { get; set; }
+
+    public virtual PaymentCustomer? PaymentCustomer { get; set; }
+
+    public virtual GuestPreference? Preference { get; set; }
+
+    public virtual Room? Room { get; set; }
+}
---
+++ SweetManagerIotWebService.API/Reservations/Domain/Model/Aggregates/Room.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using SweetManagerIotWebService.API.Reservations.Domain.Model.Entities;
+
+namespace SweetManagerIotWebService.API.Reservations.Domain.Model.Aggregates;
+
+public partial class Room
+{
+    public int Id { get; set; }
+
+    public int? TypeRoomId { get; set; }
+
+    public int? HotelId { get; set; }
+
+    public string? State { get; set; }
+
+    public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+
+    public virtual Hotel? Hotel { get; set; }
+
+    public virtual TypeRoom? TypeRoom { get; set; }
+}
