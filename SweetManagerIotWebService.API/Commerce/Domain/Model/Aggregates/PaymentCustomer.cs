3e4b7e827fa30c6ab162d831031354ca37edcf9e
Mauricio <mauriciorojasanchez@gmail.com>
2025-04-10 22:34:01 -0500

    feat(commerce): added aggregates for commerce bounded context.

---
+++ SweetManagerIotWebService.API/Commerce/Domain/Model/Aggregates/PaymentCustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using SweetManagerIotWebService.API.IAM.Domain.Model.Aggregates;
+
+namespace SweetManagerIotWebService.API.Commerce.Domain.Model.Aggregates;
+
+public partial class PaymentCustomer
+{
+    public int Id { get; set; }
+
+    public int? GuestId { get; set; }
+
+    public decimal? FinalAmount { get; set; }
+
+    public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+
+    public virtual Guest? Guest { get; set; }
+}
