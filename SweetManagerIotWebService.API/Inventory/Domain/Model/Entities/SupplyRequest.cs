d27779385bf7ca4878828930bdf665e755821476
Mauricio <mauriciorojasanchez@gmail.com>
2025-04-10 22:37:42 -0500

    feat(inventory): added entities.

---
+++ SweetManagerIotWebService.API/Inventory/Domain/Model/Entities/SupplyRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using SweetManagerIotWebService.API.Commerce.Domain.Model.Aggregates;
+
+namespace SweetManagerIotWebService.API.Inventory.Domain.Model.Entities;
+
+public partial class SupplyRequest
+{
+    public int Id { get; set; }
+
+    public int? PaymentOwnerId { get; set; }
+
+    public int? SupplyId { get; set; }
+
+    public int? Count { get; set; }
+
+    public decimal? Amount { get; set; }
+
+    public virtual PaymentOwner? PaymentOwner { get; set; }
+
+    public virtual Supply? Supply { get; set; }
+}
