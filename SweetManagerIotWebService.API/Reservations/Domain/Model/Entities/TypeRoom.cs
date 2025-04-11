b2edd2af76cdbb2077bc13a2bdf6385e4b6c7c26
Mauricio <mauriciorojasanchez@gmail.com>
2025-04-10 22:38:45 -0500

    feat(reservations): added entities.

---
+++ SweetManagerIotWebService.API/Reservations/Domain/Model/Entities/TypeRoom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using SweetManagerIotWebService.API.Reservations.Domain.Model.Aggregates;
+
+namespace SweetManagerIotWebService.API.Reservations.Domain.Model.Entities;
+
+public partial class TypeRoom
+{
+    public int Id { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal? Price { get; set; }
+
+    public virtual ICollection<Room> Rooms { get; set; } = new List<Room>();
+}
