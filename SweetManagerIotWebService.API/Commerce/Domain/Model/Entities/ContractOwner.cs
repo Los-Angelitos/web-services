f70a2cc089f752b95370fa7e279e0e9a10513baa
Mauricio <mauriciorojasanchez@gmail.com>
2025-04-10 22:34:55 -0500

    feat(commerce): added entities for commerce bounded context.

---
+++ SweetManagerIotWebService.API/Commerce/Domain/Model/Entities/ContractOwner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using SweetManagerIotWebService.API.IAM.Domain.Model.Aggregates;
+
+namespace SweetManagerIotWebService.API.Commerce.Domain.Model.Entities;
+
+public partial class ContractOwner
+{
+    public int Id { get; set; }
+
+    public int? OwnerId { get; set; }
+
+    public DateTime? StartDate { get; set; }
+
+    public DateTime? FinalDate { get; set; }
+
+    public int? SubscriptionId { get; set; }
+
+    public string? Status { get; set; }
+
+    public virtual Owner? Owner { get; set; }
+
+    public virtual Subscription? Subscription { get; set; }
+}
