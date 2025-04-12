c3ec1107d07d1b7f31bf68e43c7fb959824ade4f
Salvlormonch1 <salvlormonch@gmail.com>
2025-04-12 14:21:03 -0500

    feat(commerce): add subscription repository interface.

---
+++ SweetManagerIotWebService.API/Commerce/Domain/Repositories/ISubscriptionRepository.cs
@@ -0,0 +1,11 @@
+﻿using SweetManagerIotWebService.API.Commerce.Domain.Model.Aggregates;
+using SweetManagerIotWebService.API.Commerce.Domain.Model.ValueObjects;
+
+namespace SweetManagerIotWebService.API.Commerce.Domain.Repositories;
+
+public interface ISubscriptionRepository
+{
+    Task<IEnumerable<Subscription>> FindByNameAsync(ESubscriptionTypes name);
+    
+    Task<IEnumerable<Subscription>> FindByStatusAsync(EStates status);
+}
