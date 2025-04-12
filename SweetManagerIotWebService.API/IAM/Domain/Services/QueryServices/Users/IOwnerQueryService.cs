06535e946f75cc93218cab8adc0b8a2bfc7fdea3
Mauricio <mauriciorojasanchez@gmail.com>
2025-04-12 18:20:19 -0500

    feat(iam): added user query services.

---
+++ SweetManagerIotWebService.API/IAM/Domain/Services/QueryServices/Users/IOwnerQueryService.cs
@@ -0,0 +1,15 @@
+﻿using SweetManagerIotWebService.API.IAM.Domain.Model.Aggregates;
+using SweetManagerIotWebService.API.IAM.Domain.Model.Queries.Users;
+
+namespace SweetManagerIotWebService.API.IAM.Domain.Services.QueryServices.Users
+{
+    public interface IOwnerQueryService
+    {
+        Task<IEnumerable<Owner>> Handle(GetAllUsersQuery query);
+
+        Task<Owner?> Handle(GetUserByIdQuery query);
+
+        Task<Owner?> Handle(GetOwnerFromAnOrganizationQuery query);
+        
+    }
+}
