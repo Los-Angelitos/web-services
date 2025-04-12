33e5a73647917f942148b3d1b0c98df708762df8
Giacomo202210029 <u202210029@upc.edu.pe>
2025-04-12 00:14:28 -0500

    feat:  Added Create Room Command

---
+++ SweetManagerIotWebService.API/Reservations/Domain/Commands/Room/CreateRoomCommand.cs
@@ -0,0 +1,7 @@
+﻿using SweetManagerIotWebService.API.Reservations.Domain.Model.Entities;
+
+namespace SweetManagerIotWebService.API.Reservations.Domain.Commands.Room;
+
+public record CreateRoomCommand(
+    int Id, int? TypeRoomId, 
+    int? HotelId, string? State, ICollection<Booking> Bookings, Hotel? Hotel, TypeRoom? TypeRoom);
