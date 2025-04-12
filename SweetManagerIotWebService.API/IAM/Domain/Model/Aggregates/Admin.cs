05aebe8ba77079957585885a35e533b5e6d5e2b7
Mauricio <mauriciorojasanchez@gmail.com>
2025-04-12 15:00:54 -0500

    chore(iam): added audit for admin aggregate.

--- SweetManagerIotWebService.API/IAM/Domain/Model/Aggregates/Admin.cs
+++ SweetManagerIotWebService.API/IAM/Domain/Model/Aggregates/Admin.cs
@@ -1,26 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using SweetManagerIotWebService.API.IAM.Domain.Model.Entities;
+using SweetManagerIotWebService.API.IAM.Domain.Model.Entities.Credentials;
 
 namespace SweetManagerIotWebService.API.IAM.Domain.Model.Aggregates;
 
 public partial class Admin
 {
-    public int Id { get; set; }
+    public int Id { get; private set; }
 
-    public string? Name { get; set; }
+    public string? Name { get; private set; }
 
-    public string? Surname { get; set; }
+    public string? Surname { get; private set; }
 
-    public string? Phone { get; set; }
+    public string? Phone { get; private set; }
 
-    public string? Email { get; set; }
+    public string? Email { get; private set; }
 
-    public int? RoleId { get; set; }
+    public string? State { get; private set; }
 
-    public DateTime? CreatedAt { get; set; }
-
-    public DateTime? UpdatedAt { get; set; }
+    public int? RoleId { get; private set; }
 
     public virtual AdminCredential? AdminCredential { get; set; }
 
