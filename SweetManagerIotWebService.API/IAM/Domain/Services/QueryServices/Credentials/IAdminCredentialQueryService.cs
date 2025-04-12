be83f4fcc502ffad1a3861be1a0ce44bcb3baea2
Mauricio <mauriciorojasanchez@gmail.com>
2025-04-12 17:33:00 -0500

    feat(iam): added user credentials query services.

---
+++ SweetManagerIotWebService.API/IAM/Domain/Services/QueryServices/Credentials/IAdminCredentialQueryService.cs
@@ -0,0 +1,9 @@
+﻿using SweetManagerIotWebService.API.IAM.Domain.Model.Entities.Credentials;
+
+namespace SweetManagerIotWebService.API.IAM.Domain.Services.QueryServices.Credentials
+{
+    public interface IAdminCredentialQueryService
+    {
+        Task<AdminCredential?> FindByIdAsync(int Id);
+    }
+}
---
+++ SweetManagerIotWebService.API/IAM/Domain/Services/QueryServices/Credentials/IGuestCredentialQueryService.cs
@@ -0,0 +1,9 @@
+﻿using SweetManagerIotWebService.API.IAM.Domain.Model.Entities.Credentials;
+
+namespace SweetManagerIotWebService.API.IAM.Domain.Services.QueryServices.Credentials
+{
+    public interface IGuestCredentialQueryService
+    {
+        Task<GuestCredential> FindByIdAsync(int Id);
+    }
+}
