58fd30c861361ddfa788c8b72eac641b0322fd88
Salvlormonch1 <salvlormonch@gmail.com>
2025-04-12 14:12:06 -0500

    feat(commerce): add contract owner repository interface.

---
+++ SweetManagerIotWebService.API/Commerce/Domain/Repositories/IContractOwnerRepository.cs
@@ -0,0 +1,10 @@
+﻿using SweetManagerIotWebService.API.Commerce.Domain.Model.Entities;
+
+namespace SweetManagerIotWebService.API.Commerce.Domain.Repositories;
+
+public interface IContractOwnerRepository
+{
+    Task<IEnumerable<ContractOwner>> FindByOwnerIdAsync(int ownerId);
+    
+    Task<IEnumerable<ContractOwner>> FindBySubscriptionIdAsync(int subscriptionId);
+}
