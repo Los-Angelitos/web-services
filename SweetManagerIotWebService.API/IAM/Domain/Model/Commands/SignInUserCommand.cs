eba3e2eca934400620f34cf7763e6b793d85c5f2
Mauricio <mauriciorojasanchez@gmail.com>
2025-04-12 15:04:28 -0500

    feat(iam): added commands for authentication.

---
+++ SweetManagerIotWebService.API/IAM/Domain/Model/Commands/SignInUserCommand.cs
@@ -0,0 +1,5 @@
+﻿namespace SweetManagerIotWebService.API.IAM.Domain.Model.Commands
+{
+    public record SignInUserCommand(string email, string password, string role);
+
+}
---
+++ SweetManagerIotWebService.API/IAM/Domain/Model/Commands/SignUpUserCommand.cs
@@ -0,0 +1,4 @@
+﻿namespace SweetManagerIotWebService.API.IAM.Domain.Model.Commands
+{
+    public record SignUpUserCommand(int Id, string Name, string Surname, string Phone, string Email, int RoleId);
+}
