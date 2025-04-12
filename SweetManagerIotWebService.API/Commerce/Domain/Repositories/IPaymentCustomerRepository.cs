c5184f1eaefd5349c6141685eda45f1aee585452
Salvlormonch1 <salvlormonch@gmail.com>
2025-04-12 14:21:36 -0500

    feat(commerce): add payment customer repository interface.

---
+++ SweetManagerIotWebService.API/Commerce/Domain/Repositories/IPaymentCustomerRepository.cs
@@ -0,0 +1,8 @@
+﻿using SweetManagerIotWebService.API.Commerce.Domain.Model.Aggregates;
+
+namespace SweetManagerIotWebService.API.Commerce.Domain.Repositories;
+
+public interface IPaymentCustomerRepository
+{
+    Task<IEnumerable<PaymentCustomer>> FindByCustomerIdAsync(int customerId);
+}
