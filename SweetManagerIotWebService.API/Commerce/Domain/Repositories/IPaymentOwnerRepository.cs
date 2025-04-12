1bfd0d5bd27dc2f2a72ef651e3fd1c0c707a57c8
Salvlormonch1 <salvlormonch@gmail.com>
2025-04-12 14:15:30 -0500

    feat(commerce): add payment owner repository interface.

---
+++ SweetManagerIotWebService.API/Commerce/Domain/Repositories/IPaymentOwnerRepository.cs
@@ -0,0 +1,8 @@
+﻿using SweetManagerIotWebService.API.Commerce.Domain.Model.Aggregates;
+
+namespace SweetManagerIotWebService.API.Commerce.Domain.Repositories;
+
+public interface IPaymentOwnerRepository
+{
+    Task<IEnumerable<PaymentOwner>> FindByOwnerIdAsync(int ownerId);
+}
