4ad4d176a99cb863233814e840d27c38c07a131a
Mauricio <mauriciorojasanchez@gmail.com>
2025-04-12 16:53:52 -0500

    feat(iam): added role repository.

---
+++ SweetManagerIotWebService.API/IAM/Domain/Repositories/Roles/IRoleRepository.cs
@@ -0,0 +1,14 @@
+﻿using SweetManagerIotWebService.API.IAM.Domain.Model.Entities.Roles;
+using SweetManagerIotWebService.API.Shared.Domain.Repositories;
+
+namespace SweetManagerIotWebService.API.IAM.Domain.Repositories.Roles
+{
+    public interface IRoleRepository : IBaseRepository<Role>
+    {
+        Task<IEnumerable<Role>> FindAllAsync();
+
+        Task<Role?> FindByNameAsync();
+
+        Task<int?> FindIdByNameAsync();
+    }
+}
