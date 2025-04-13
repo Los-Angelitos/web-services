849aa0ebcc56291cc4c8e956af15f66840db0ae0
Giacomo202210029 <u202210029@upc.edu.pe>
2025-04-13 01:57:34 -0500

    chore:  Added Room Initializers

--- SweetManagerIotWebService.API/Reservations/Domain/Model/Aggregates/Room.cs
+++ SweetManagerIotWebService.API/Reservations/Domain/Model/Aggregates/Room.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using SweetManagerIotWebService.API.Reservations.Domain.Commands.Room;
+using SweetManagerIotWebService.API.Reservations.Domain.Model.Commands.Room;
 using SweetManagerIotWebService.API.Reservations.Domain.Model.Entities;
 
 namespace SweetManagerIotWebService.API.Reservations.Domain.Model.Aggregates;
@@ -7,16 +10,41 @@ namespace SweetManagerIotWebService.API.Reservations.Domain.Model.Aggregates;
 public partial class Room
 {
     public int Id { get; set; }
-
     public int? TypeRoomId { get; set; }
-
     public int? HotelId { get; set; }
-
-    public string? State { get; set; }
-
+    public string? State { get; set; } 
+    
+    [JsonIgnore]
     public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
 
     public virtual Hotel? Hotel { get; set; }
 
     public virtual TypeRoom? TypeRoom { get; set; }
+
+    public Room()
+    {
+        this.TypeRoomId = 0;
+        this.HotelId = 0;
+        this.State = string.Empty;
+    }
+    public Room(int id, int typeRoomId, int hotelId, string state)
+    {
+        Id = id;
+        TypeRoomId = typeRoomId;
+        HotelId = hotelId;
+        State = state.ToUpper(); 
+    }
+
+    public Room(CreateRoomCommand command)
+    {
+        TypeRoomId = command.TypeRoomId;
+        HotelId = command.HotelId;
+        State = command.State?.ToUpper();
+    }
+
+    public Room(UpdateRoomStateCommand command)
+    {
+        this.Id = command.Id;
+        this.State = command.State;
+    }
 }
