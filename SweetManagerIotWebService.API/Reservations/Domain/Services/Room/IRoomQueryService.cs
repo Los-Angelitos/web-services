c9553909b363a4cde4fbdb3f80938c71da77f4ec
Giacomo202210029 <u202210029@upc.edu.pe>
2025-04-13 01:55:54 -0500

    feat:  Added Room Query Services

---
+++ SweetManagerIotWebService.API/Reservations/Domain/Services/Room/IRoomQueryService.cs
@@ -0,0 +1,14 @@
+﻿using SweetManagerIotWebService.API.Reservations.Domain.Model.Queries;
+
+namespace SweetManagerIotWebService.API.Reservations.Domain.Services.Room;
+
+public interface IRoomQueryService
+{
+    Task<IEnumerable<Model.Aggregates.Room>> Handle(GetAllRoomsQuery query);
+
+    Task<Model.Aggregates.Room?> Handle(GetRoomsByIdQuery query);
+
+    Task<IEnumerable<Model.Aggregates.Room>> Handle(GetRoomsByStateQuery query);
+
+    Task<IEnumerable<Model.Aggregates.Room>> Handle(GetRoomsByTypeRoomIdQuery query); 
+}
