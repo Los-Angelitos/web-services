1f8d6ce251d8cdad768899ba641553b447d9edb1
Giacomo202210029 <u202210029@upc.edu.pe>
2025-04-13 02:44:50 -0500

    chore:  Added TypeRoom

--- SweetManagerIotWebService.API/Reservations/Domain/Model/Entities/TypeRoom.cs
+++ SweetManagerIotWebService.API/Reservations/Domain/Model/Entities/TypeRoom.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using SweetManagerIotWebService.API.Reservations.Domain.Model.Aggregates;
+using SweetManagerIotWebService.API.Reservations.Domain.Model.Commands.TypeRoom;
 
 namespace SweetManagerIotWebService.API.Reservations.Domain.Model.Entities;
 
@@ -10,7 +11,26 @@ public partial class TypeRoom
 
     public string? Description { get; set; }
 
-    public decimal? Price { get; set; }
+    public decimal Price { get; set; }
 
     public virtual ICollection<Room> Rooms { get; set; } = new List<Room>();
+
+    public TypeRoom()
+    {
+        this.Description = string.Empty;
+        this.Price = 0;
+    }
+    
+    public TypeRoom(int id, string description, decimal price)
+    {
+        Id = id;
+        Description = description;
+        Price = price;
+    }
+    
+    public TypeRoom(CreateTypeRoomCommand command)
+    {
+        Description = command.Description;
+        Price = command.Price;
+    }
 }
