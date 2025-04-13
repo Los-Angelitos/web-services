22792ac2d69b1b5d7bf4c40ecb4ff7a21eda9e2c
Salvlormonch1 <salvlormonch@gmail.com>
2025-04-13 12:24:58 -0500

    feat(commerce): add contract owner command service implementation.

--- SweetManagerIotWebService.API/Commerce/Domain/Model/Entities/ContractOwner.cs
+++ SweetManagerIotWebService.API/Commerce/Domain/Model/Entities/ContractOwner.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using SweetManagerIotWebService.API.Commerce.Domain.Model.Aggregates;
+using SweetManagerIotWebService.API.Commerce.Domain.Model.Commands;
 using SweetManagerIotWebService.API.Commerce.Domain.Model.ValueObjects;
 using SweetManagerIotWebService.API.IAM.Domain.Model.Aggregates;
 
@@ -23,4 +24,37 @@ public partial class ContractOwner
     public virtual Owner? Owner { get; set; }
 
     public virtual Subscription? Subscription { get; set; }
+    
+    public ContractOwner(
+        int? ownerId, 
+        DateTime? startDate, 
+        DateTime? finalDate, 
+        int? subscriptionId, 
+        EStates status)
+    {
+        OwnerId = ownerId;
+        StartDate = startDate;
+        FinalDate = finalDate;
+        SubscriptionId = subscriptionId;
+        Status = status;
+    }
+    
+    public ContractOwner(CreateContractOwnerCommand command)
+    {
+        OwnerId = command.OwnerId;
+        StartDate = command.StartDate;
+        FinalDate = command.FinalDate;
+        SubscriptionId = command.SubscriptionId;
+        Status = command.Status;
+    }
+    
+    public ContractOwner(UpdateContractOwnerCommand command)
+    {
+        Id = command.Id;
+        OwnerId = command.OwnerId;
+        StartDate = command.StartDate;
+        FinalDate = command.FinalDate;
+        SubscriptionId = command.SubscriptionId;
+        Status = command.Status;
+    }
 }
