5fba7a529a6b8727313b4ed66719e44bab8d2ecd
Arian Rodriguez <arianmrv12@gmail.com>
2025-04-13 14:16:53 -0500

    add(org-management): validation for the hotel resource

--- SweetManagerIotWebService.API/OrganizationalManagement/Interfaces/REST/Transform/CreateHotelCommandFromResourceAssembler.cs
+++ SweetManagerIotWebService.API/OrganizationalManagement/Interfaces/REST/Transform/CreateHotelCommandFromResourceAssembler.cs
@@ -1,5 +1,6 @@
 ﻿using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Model.Commands;
 using SweetManagerIotWebService.API.OrganizationalManagement.Interfaces.REST.Resources;
+using SweetManagerIotWebService.API.OrganizationalManagement.Interfaces.REST.Validation;
 
 namespace SweetManagerIotWebService.API.OrganizationalManagement.Interfaces.REST.Transform;
 
@@ -7,6 +8,7 @@ public class CreateHotelCommandFromResourceAssembler
 {
     public static CreateHotelCommand ToCommandFromResource(CreateHotelResource resource)
     {
+        CreateHotelResourceValidator.Validate(resource);
         return new CreateHotelCommand(resource.OwnerId, resource.Name, resource.Description, resource.Email, resource.Address, resource.Phone);
     }
 }
