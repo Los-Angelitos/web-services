7d75b0f3f8d9541d7026d682e0bab25b9989563b
Fabia Herrera <herrerafabia.190604@gmail.com>
2025-04-13 19:14:15 -0500

    feat(communication): added the implementation of the notification repository.

---
+++ SweetManagerIotWebService.API/Communication/Infrastructure/Persistence/EFC/Repositories/NotificationRepository.cs
@@ -0,0 +1,11 @@
+﻿using SweetManagerIotWebService.API.Communication.Domain.Model.Aggregates;
+using SweetManagerIotWebService.API.Communication.Domain.Repositories;
+using SweetManagerIotWebService.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using SweetManagerIotWebService.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace SweetManagerIotWebService.API.Communication.Infrastructure.Persistence.EFC.Repositories;
+
+public class NotificationRepository(SweetManagerContext context) : BaseRepository<Notification>(context), INotificationRepository
+{
+    
+}
