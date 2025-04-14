a8f38c82af4367b8ca4e9412c3a1cf336813d0e8
Fabia Herrera <herrerafabia.190604@gmail.com>
2025-04-13 19:14:05 -0500

    feat(communication): added the interface of the notification repository.

---
+++ SweetManagerIotWebService.API/Communication/Domain/Repositories/INotificationRepository.cs
@@ -0,0 +1,9 @@
+﻿using SweetManagerIotWebService.API.Communication.Domain.Model.Aggregates;
+using SweetManagerIotWebService.API.Shared.Domain.Repositories;
+
+namespace SweetManagerIotWebService.API.Communication.Domain.Repositories;
+
+public interface INotificationRepository : IBaseRepository<Notification>
+{
+    
+}
