da5bcba1de8a60e14f7592aadcfc26a3b9aa32c1
Omar Morales <andrewmm45333@gmail.com>
2025-04-14 21:37:07 -0500

    feat(Inventory): added dependency injection.

--- SweetManagerIotWebService.API/Program.cs
+++ SweetManagerIotWebService.API/Program.cs
@@ -6,6 +6,11 @@ using SweetManagerIotWebService.API.Shared.Infrastructure.Interfaces.ASP.Configu
 using SweetManagerIotWebService.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 using SweetManagerIotWebService.API.Shared.Infrastructure.Persistence.EFC.Repositories;
 using System.Data;
+using SweetManagerIotWebService.API.Inventory.Application.Internal.CommandServices;
+using SweetManagerIotWebService.API.Inventory.Application.Internal.QueryServices;
+using SweetManagerIotWebService.API.Inventory.Domain.Repositories;
+using SweetManagerIotWebService.API.Inventory.Domain.Services;
+using SweetManagerIotWebService.API.Inventory.Infrastructure.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -117,8 +122,13 @@ builder.Services.AddHttpContextAccessor();
 
 
 // Inventory Bounded Context
+builder.Services.AddScoped<ISupplyRepository, SupplyRepository>(); 
+builder.Services.AddScoped<ISupplyCommandService, SupplyCommandService>();
+builder.Services.AddScoped<ISupplyQueryService, SupplyQueryService>();
 
-
+builder.Services.AddScoped<ISupplyRequestRepository, SupplyRequestRepository>();
+builder.Services.AddScoped<ISupplyRequestCommandService, SupplyRequestCommandService>();
+builder.Services.AddScoped<ISupplyRequestQueryService, SupplyRequestQueryService>();
 // Communication Bounded context
 
 
