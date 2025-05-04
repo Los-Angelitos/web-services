f1a2bae88fb79f894f455fe78e87dcd18930a1d3
Mauricio <mauriciorojasanchez@gmail.com>
2025-05-04 03:23:38 -0500

    feat(iam): added role repository.

---
+++ SweetManagerIotWebService.API/IAM/Infrastructure/Persistence/EFC/Repositories/Roles/RoleRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SweetManagerIotWebService.API.IAM.Domain.Model.Entities.Roles;
+using SweetManagerIotWebService.API.IAM.Domain.Repositories.Roles;
+using SweetManagerIotWebService.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using SweetManagerIotWebService.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace SweetManagerIotWebService.API.IAM.Infrastructure.Persistence.EFC.Repositories.Roles
+{
+    public class RoleRepository(SweetManagerContext context) : BaseRepository<Role>(context), IRoleRepository
+    {
+        public async Task<Role?> FindByNameAsync(string name)
+        => await Context.Set<Role>().Where(r => r.Name.Equals(name)).FirstOrDefaultAsync();
+
+        public async Task<int?> FindIdByNameAsync(string name)
+        {
+            var result = await Context.Set<Role>().Where(r => r.Name.Equals(name)).FirstOrDefaultAsync();
+
+            return result?.Id;
+        }
+    }
+}
