be57f97a4c4148a4f93f86f39037a6143738a21d
Mauricio <mauriciorojasanchez@gmail.com>
2025-05-04 03:10:07 -0500

    feat(iam): added role command service.

---
+++ SweetManagerIotWebService.API/IAM/Application/Internal/CommandServices/Roles/RoleCommandService.cs
@@ -0,0 +1,28 @@
+﻿using SweetManagerIotWebService.API.IAM.Domain.Model.Commands.Roles;
+using SweetManagerIotWebService.API.IAM.Domain.Model.Entities.Roles;
+using SweetManagerIotWebService.API.IAM.Domain.Model.ValueObjects;
+using SweetManagerIotWebService.API.IAM.Domain.Repositories.Roles;
+using SweetManagerIotWebService.API.IAM.Domain.Services.CommandServices.Roles;
+using SweetManagerIotWebService.API.Shared.Domain.Repositories;
+
+namespace SweetManagerIotWebService.API.IAM.Application.Internal.CommandServices.Roles
+{
+    public class RoleCommandService(IRoleRepository roleRepository,
+        IUnitOfWork unitOfWork) : IRoleCommandService
+    {
+        public async Task<bool> Handle(SeedRolesCommand command)
+        {
+            foreach (var role in Enum.GetValues(typeof(ERoles)))
+            {
+                if (await roleRepository.FindByNameAsync(role.ToString()!) is null)
+                {
+                    await roleRepository.AddAsync(new Role(role.ToString()!));
+                }
+            }
+
+            await unitOfWork.CommitAsync();
+
+            return true;
+        }
+    }
+}
