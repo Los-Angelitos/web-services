4cc66b0bfda2eefefbb77a341a0fdc224fed5e91
Mauricio <mauriciorojasanchez@gmail.com>
2025-05-04 03:29:04 -0500

    chore(iam): updated dependencies and injections.

--- SweetManagerIotWebService.API/Program.cs
+++ SweetManagerIotWebService.API/Program.cs
@@ -1,11 +1,44 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using MySql.Data.MySqlClient;
+using SweetManagerIotWebService.API.IAM.Application.Internal.CommandServices.Credentials;
+using SweetManagerIotWebService.API.IAM.Application.Internal.CommandServices.Preferences;
+using SweetManagerIotWebService.API.IAM.Application.Internal.CommandServices.Roles;
+using SweetManagerIotWebService.API.IAM.Application.Internal.CommandServices.Users;
+using SweetManagerIotWebService.API.IAM.Application.Internal.OutboundServices;
+using SweetManagerIotWebService.API.IAM.Application.Internal.QueryServices.Credentials;
+using SweetManagerIotWebService.API.IAM.Application.Internal.QueryServices.Preferences;
+using SweetManagerIotWebService.API.IAM.Application.Internal.QueryServices.Roles;
+using SweetManagerIotWebService.API.IAM.Application.Internal.QueryServices.Users;
+using SweetManagerIotWebService.API.IAM.Domain.Repositories.Credentials;
+using SweetManagerIotWebService.API.IAM.Domain.Repositories.Preferences;
+using SweetManagerIotWebService.API.IAM.Domain.Repositories.Roles;
+using SweetManagerIotWebService.API.IAM.Domain.Repositories.Users;
+using SweetManagerIotWebService.API.IAM.Domain.Services.CommandServices.Credentials;
+using SweetManagerIotWebService.API.IAM.Domain.Services.CommandServices.Preferences;
+using SweetManagerIotWebService.API.IAM.Domain.Services.CommandServices.Roles;
+using SweetManagerIotWebService.API.IAM.Domain.Services.CommandServices.Users;
+using SweetManagerIotWebService.API.IAM.Domain.Services.QueryServices.Credentials;
+using SweetManagerIotWebService.API.IAM.Domain.Services.QueryServices.Preferences;
+using SweetManagerIotWebService.API.IAM.Domain.Services.QueryServices.Roles;
+using SweetManagerIotWebService.API.IAM.Domain.Services.QueryServices.Users;
+using SweetManagerIotWebService.API.IAM.Infrastructure.Hashing.Argon2Id.Services;
+using SweetManagerIotWebService.API.IAM.Infrastructure.Persistence.EFC.Repositories.Credentials;
+using SweetManagerIotWebService.API.IAM.Infrastructure.Persistence.EFC.Repositories.Preferences;
+using SweetManagerIotWebService.API.IAM.Infrastructure.Persistence.EFC.Repositories.Roles;
+using SweetManagerIotWebService.API.IAM.Infrastructure.Persistence.EFC.Repositories.Users;
+using SweetManagerIotWebService.API.IAM.Infrastructure.Pipeline.Middleware.Extensions;
+using SweetManagerIotWebService.API.IAM.Infrastructure.Population.Roles;
+using SweetManagerIotWebService.API.IAM.Infrastructure.Tokens.JWT.Configuration;
+using SweetManagerIotWebService.API.IAM.Infrastructure.Tokens.JWT.Services;
 using SweetManagerIotWebService.API.Shared.Domain.Repositories;
 using SweetManagerIotWebService.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
 using SweetManagerIotWebService.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 using SweetManagerIotWebService.API.Shared.Infrastructure.Persistence.EFC.Repositories;
 using System.Data;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -108,7 +141,33 @@ builder.Services.AddHttpContextAccessor();
 #region Dependency Injection
 
 // IAM Bounded context
-
+builder.Services.AddScoped<IAdminCredentialRepository, AdminCredentialRepository>();
+builder.Services.AddScoped<IGuestCredentialRepository, GuestCredentialRepository>();
+builder.Services.AddScoped<IOwnerCredentialRepository, OwnerCredentialRepository>();
+builder.Services.AddScoped<IGuestPreferenceRepository, GuestPreferenceRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<IAdminRepository, AdminRepository>();
+builder.Services.AddScoped<IGuestRepository, GuestRepository>();
+builder.Services.AddScoped<IOwnerRepository, OwnerRepository>();
+builder.Services.AddScoped<IAdminCredentialCommandService, AdminCredentialCommandService>();
+builder.Services.AddScoped<IGuestCredentialCommandService, GuestCredentialCommandService>();
+builder.Services.AddScoped<IOwnerCredentialCommandService, OwnerCredentialCommandService>();
+builder.Services.AddScoped<IGuestPreferenceCommandService, GuestPreferenceCommandService>();
+builder.Services.AddScoped<IRoleCommandService, RoleCommandService>();
+builder.Services.AddScoped<IAdminCommandService, AdminCommandService>();
+builder.Services.AddScoped<IGuestCommandService, GuestCommandService>();
+builder.Services.AddScoped<IOwnerCommandService, OwnerCommandService>();
+builder.Services.AddScoped<IAdminCredentialQueryService, AdminCredentialQueryService>();
+builder.Services.AddScoped<IGuestCredentialQueryService, GuestCredentialQueryService>();
+builder.Services.AddScoped<IOwnerCredentialQueryService, OwnerCredentialQueryService>();
+builder.Services.AddScoped<IGuestPreferenceQueryService, GuestPreferenceQueryService>();
+builder.Services.AddScoped<IRoleQueryService, RoleQueryService>();
+builder.Services.AddScoped<IAdminQueryService, AdminQueryService>();
+builder.Services.AddScoped<IGuestQueryService, GuestQueryService>();
+builder.Services.AddScoped<IOwnerQueryService, OwnerQueryService>();
+builder.Services.AddScoped<IHashingService, HashingService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<RolesInitializer>();
 
 // Reservations bounded context
 
@@ -134,7 +193,47 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 #region JWT Configuration
 
-// In progress
+var tokenSettings = builder.Configuration.GetSection("JwtSettings");
+
+builder.Services.Configure<TokenSettings>(tokenSettings);
+
+var secretKey = tokenSettings["SecretKey"];
+
+var audience = tokenSettings["Audience"];
+
+var issuer = tokenSettings["Issuer"];
+
+var securityKey = !string.IsNullOrEmpty(secretKey)
+    ? new SymmetricSecurityKey(Encoding.Default.GetBytes(secretKey))
+    : throw new ArgumentException("Secret key is null or empty");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        IssuerSigningKey = securityKey,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddScoped<IHashingService, HashingService>();
+
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+builder.Services.AddTransient<TokenValidationHandler>();
+
+builder.Services.AddAuthorization();
 
 #endregion
 
@@ -154,7 +253,9 @@ using (var scope = app.Services.CreateScope())
 #region Run DatabaseInitializer
 using (var scope = app.Services.CreateScope())
 {
-    // In progress
+    var roleInitializer = scope.ServiceProvider.GetRequiredService<RolesInitializer>();
+
+    roleInitializer.InitializeAsync().Wait();
 }
 #endregion
 
@@ -164,7 +265,12 @@ app.UseCors(
 );
 
 app.UseSwagger();
+
 app.UseSwaggerUI();
+
+app.UseRouting();
+
+app.UseRequestAuthorization();
 
 app.UseHttpsRedirection();
 
@@ -174,4 +280,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+app.Run(); 
