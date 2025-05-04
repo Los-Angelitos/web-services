f58ca5fcece16c38a9a9b70ce9118c1cd1a8d709
Mauricio <mauriciorojasanchez@gmail.com>
2025-05-04 03:13:52 -0500

    feat(iam): added update behavior for owner aggregate.

--- SweetManagerIotWebService.API/IAM/Domain/Model/Aggregates/Owner.cs
+++ SweetManagerIotWebService.API/IAM/Domain/Model/Aggregates/Owner.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using SweetManagerIotWebService.API.Commerce.Domain.Model.Aggregates;
 using SweetManagerIotWebService.API.Commerce.Domain.Model.Entities;
+using SweetManagerIotWebService.API.IAM.Domain.Model.Commands.Authentication;
 using SweetManagerIotWebService.API.IAM.Domain.Model.Entities.Credentials;
 using SweetManagerIotWebService.API.IAM.Domain.Model.Entities.Roles;
 
@@ -32,4 +33,39 @@ public partial class Owner
     public virtual ICollection<PaymentOwner> PaymentOwners { get; set; } = new List<PaymentOwner>();
 
     public virtual Role? Role { get; set; }
+
+    public Owner() { }
+
+    public Owner(int id, string name, string surname, string phone, string email, string state, int roleId)
+    {
+        Id = id;
+        Name = name;
+        Surname = surname;
+        Phone = phone;
+        Email = email;
+        State = state;
+        RoleId = roleId;
+    }
+
+    public Owner(UpdateUserCommand command)
+    {
+        Id = command.Id;
+        Name = command.Name;
+        Surname = command.Surname;
+        Phone = command.Phone;
+        Email = command.Email;
+        State = command.State;
+    }
+
+    public Owner Update(UpdateUserCommand command)
+    {
+        Id = command.Id;
+        Name = command.Name;
+        Surname = command.Surname;
+        Phone = command.Phone;
+        Email = command.Email;
+        State = command.State;
+
+        return this;
+    }
 }
