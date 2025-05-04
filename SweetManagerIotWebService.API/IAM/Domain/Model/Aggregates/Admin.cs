0ad74a6580bfc1fcf456fe90bd3b0dcaa474c695
Mauricio <mauriciorojasanchez@gmail.com>
2025-05-04 03:13:05 -0500

    chore(iam): added update behavior for admin aggregate.

--- SweetManagerIotWebService.API/IAM/Domain/Model/Aggregates/Admin.cs
+++ SweetManagerIotWebService.API/IAM/Domain/Model/Aggregates/Admin.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using SweetManagerIotWebService.API.IAM.Domain.Model.Commands.Authentication;
 using SweetManagerIotWebService.API.IAM.Domain.Model.Entities.Credentials;
 using SweetManagerIotWebService.API.IAM.Domain.Model.Entities.Roles;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace SweetManagerIotWebService.API.IAM.Domain.Model.Aggregates;
 
@@ -21,7 +21,46 @@ public partial class Admin
 
     public int? RoleId { get; private set; }
 
+    public int? HotelId { get; private set; }
+
     public virtual AdminCredential? AdminCredential { get; set; }
 
     public virtual Role? Role { get; set; }
+
+    public virtual Hotel? Hotel { get; set; }
+
+    public Admin() { }
+
+    public Admin(int id, string name, string surname, string phone, string email, string state, int roleId)
+    {
+        Id = id;
+        Name = name;
+        Surname = surname;
+        Phone = phone;
+        Email = email;
+        State = state;
+        RoleId = roleId;
+    }
+
+    public Admin(UpdateUserCommand command)
+    {
+        Id = command.Id;
+        Name = command.Name;
+        Surname = command.Surname;
+        Phone = command.Phone;
+        Email = command.Email;
+        State = command.State;
+    }
+
+    public Admin Update(UpdateUserCommand command)
+    {
+        Id = command.Id;
+        Name = command.Name;
+        Surname = command.Surname;
+        Phone = command.Phone;
+        Email = command.Email;
+        State = command.State;
+
+        return this;
+    }
 }
