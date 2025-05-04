fa5fed30c8418e1458c4c6927fdb50f7201dd55c
Mauricio <mauriciorojasanchez@gmail.com>
2025-05-04 03:18:02 -0500

    chore(iam): updated user repositories.

--- SweetManagerIotWebService.API/IAM/Domain/Repositories/Users/IAdminRepository.cs
+++ SweetManagerIotWebService.API/IAM/Domain/Repositories/Users/IAdminRepository.cs
@@ -5,8 +5,10 @@ namespace SweetManagerIotWebService.API.IAM.Domain.Repositories.Users
 {
     public interface IAdminRepository : IBaseRepository<Admin>
     {
-        Task<IEnumerable<Admin>> FindAllAsync();
+        Task<dynamic?> FindAllByFiltersAsync(string? email, string? phone, string? state);
 
+        Task<IEnumerable<Admin>> FindAllByHotelIdAsync(int hotelId);
 
+        Task<int?> FindHotelIdByIdAsync(int id);
     }
 }
--- SweetManagerIotWebService.API/IAM/Domain/Repositories/Users/IGuestRepository.cs
+++ SweetManagerIotWebService.API/IAM/Domain/Repositories/Users/IGuestRepository.cs
@@ -5,6 +5,10 @@ namespace SweetManagerIotWebService.API.IAM.Domain.Repositories.Users
 {
     public interface IGuestRepository : IBaseRepository<Guest>
     {
-        Task<IEnumerable<Guest>> FindAllAsync();
+        Task<dynamic?> FindAllByFiltersAsync(string? email, string? phone, string? state);
+
+        Task<IEnumerable<Guest>> FindAllByHotelIdAsync(int hotelId);
+
+        Task<int?> FindHotelIdByIdAsync(int id);
     }
 }
