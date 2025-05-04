9bc1a15d2ec658e1a000cdd772f97562f74d431e
Salvlormonch1 <salvlormonch@gmail.com>
2025-05-04 16:53:07 -0500

    feat(commerce): added resources and transform files for dashboard methods.

---
+++ SweetManagerIotWebService.API/Commerce/Interfaces/REST/Resources/ComparativeIncomeResource.cs
@@ -0,0 +1,3 @@
+﻿namespace SweetManagerIotWebService.API.Commerce.Interfaces.REST.Resources;
+
+public record ComparativeIncomeResource(int? WeekNumbers, decimal TotalIncome, decimal TotalExpense, decimal TotalProfit);
---
+++ SweetManagerIotWebService.API/Commerce/Interfaces/REST/Transform/ComparativeIncomeResourceFromEntityAssembler.cs
@@ -0,0 +1,12 @@
+﻿using SweetManagerIotWebService.API.Commerce.Interfaces.REST.Resources;
+
+namespace SweetManagerIotWebService.API.Commerce.Interfaces.REST.Transform;
+
+public static class ComparativeIncomeResourceFromEntityAssembler
+{
+    public static ComparativeIncomeResource ToResourceFromEntity(dynamic entity)
+    {
+        return new ComparativeIncomeResource(entity.week_number, entity.total_income, entity.total_expense,
+            entity.total_profit);
+    }
+}
