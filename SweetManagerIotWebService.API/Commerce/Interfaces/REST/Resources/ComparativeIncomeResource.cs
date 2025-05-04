a6c121ebbbb52197d6012ca3e335e88f7c233380
Salvlormonch1 <salvlormonch@gmail.com>
2025-05-04 17:18:09 -0500

    fix(commerce): corrected monthly and weekly resource and transform files.

--- SweetManagerIotWebService.API/Commerce/Interfaces/REST/Resources/ComparativeIncomeResource.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace SweetManagerIotWebService.API.Commerce.Interfaces.REST.Resources;
-
-public record ComparativeIncomeResource(int? WeekNumbers, decimal TotalIncome, decimal TotalExpense, decimal TotalProfit);
