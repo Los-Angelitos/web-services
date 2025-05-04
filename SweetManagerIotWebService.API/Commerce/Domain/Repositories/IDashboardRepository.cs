a80ab898f379e83d66383e475938988b86746e86
Salvlormonch1 <salvlormonch@gmail.com>
2025-05-04 16:34:49 -0500

    feat(commerce): added dashboard repository interface.

---
+++ SweetManagerIotWebService.API/Commerce/Domain/Repositories/IDashboardRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace SweetManagerIotWebService.API.Commerce.Domain.Repositories;
+
+public interface IDashboardRepository
+{
+    Task<IEnumerable<dynamic>> FindComparativeIncomesAsync(int hotelId);
+}
