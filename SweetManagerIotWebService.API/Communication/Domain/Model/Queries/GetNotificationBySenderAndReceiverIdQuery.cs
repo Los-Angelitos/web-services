42479bdf0982d57b05a37795a719cf87e7fc1c77
Giacomo202210029 <u202210029@upc.edu.pe>
2025-05-05 23:21:14 -0500

    feat:  Added new notification queries

---
+++ SweetManagerIotWebService.API/Communication/Domain/Model/Queries/GetNotificationBySenderAndReceiverIdQuery.cs
@@ -0,0 +1,3 @@
+﻿namespace SweetManagerIotWebService.API.Communication.Domain.Model.Queries;
+
+public record GetNotificationBySenderAndReceiverIdQuery(int SenderId, int ReceiverId);
---
+++ SweetManagerIotWebService.API/Communication/Domain/Model/Queries/GetNotificationsByHotelIdQuery.cs
@@ -0,0 +1,3 @@
+﻿namespace SweetManagerIotWebService.API.Communication.Domain.Model.Queries;
+
+public record GetNotificationsByHotelIdQuery(int hotelId);
