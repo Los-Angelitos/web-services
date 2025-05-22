09427f33e1fd597df89e48049c728174bede524a
Giacomo202210029 <u202210029@upc.edu.pe>
2025-05-21 21:26:30 -0500

    feat:  Created interfaces for services

---
+++ SweetManagerIotWebService.API/Reservations/Domain/Services/Thermostat/IThermostatCommandService.cs
@@ -0,0 +1,12 @@
+﻿using SweetManagerIotWebService.API.Reservations.Domain.Model.Commands.Thermostat;
+
+namespace SweetManagerIotWebService.API.Reservations.Domain.Services.Thermostat;
+
+public interface IThermostatCommandService
+{
+    Task<bool> Handle(CreateThermostatCommand command);
+    Task<bool> Handle(UpdateThermostatStateCommand command);
+    
+    Task<bool> Handle(UpdateThermostatTemperatureCommand command);
+    Task<bool> Handle(UpdateThermostatCommand command);
+}
