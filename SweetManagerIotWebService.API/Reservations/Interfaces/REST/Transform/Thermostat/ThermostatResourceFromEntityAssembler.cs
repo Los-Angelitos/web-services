263180331a4216cabc8e234976d5759f25e4334f
Giacomo202210029 <u202210029@upc.edu.pe>
2025-05-21 21:35:50 -0500

    chore:  Added thermostat resources

---
+++ SweetManagerIotWebService.API/Reservations/Interfaces/REST/Transform/Thermostat/ThermostatResourceFromEntityAssembler.cs
@@ -0,0 +1,18 @@
+﻿using SweetManagerIotWebService.API.Reservations.Interfaces.REST.Resources.Thermostat;
+
+namespace SweetManagerIotWebService.API.Reservations.Interfaces.REST.Transform.Thermostat;
+
+public class ThermostatResourceFromEntityAssembler
+{
+    public static ThermostatResource FromEntity(Domain.Model.Aggregates.Thermostat thermostat)
+    {
+        return new ThermostatResource(
+            thermostat.Id,
+            thermostat.RoomId,
+            thermostat.IpAddress,
+            thermostat.MacAddress,
+            thermostat.Temperature,
+            thermostat.State,
+            thermostat.LastUpdate);
+    }
+}
