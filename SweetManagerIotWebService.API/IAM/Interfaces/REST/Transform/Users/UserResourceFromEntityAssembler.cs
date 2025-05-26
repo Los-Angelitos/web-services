70100f0ff823b25fd34bc450597432697c927274
Mauricio <mauriciorojasanchez@gmail.com>
2025-05-26 00:14:57 -0500

    chore: added photo url in external resources.

--- SweetManagerIotWebService.API/IAM/Interfaces/REST/Transform/Users/UserResourceFromEntityAssembler.cs
+++ SweetManagerIotWebService.API/IAM/Interfaces/REST/Transform/Users/UserResourceFromEntityAssembler.cs
@@ -7,7 +7,7 @@ namespace SweetManagerIotWebService.API.IAM.Interfaces.REST.Transform.Users
         public static UserResource ToResourceFromEntity(dynamic user)
         {
             return new UserResource(user.Id, user.Name, user.Surname, user.Phone, 
-                user.Email, user.State, user.RoleId);
+                user.Email, user.State, user.RoleId, user.PhotoURL);
         }
     }
 }
