2e46668b576b1e5ef3a3ffe45da5633c12c8c49b
Mauricio <mauriciorojasanchez@gmail.com>
2025-05-27 00:16:21 -0500

    feat: added multimedia repository.

---
+++ SweetManagerIotWebService.API/OrganizationalManagement/Infrastructure/Persistence/EFC/Repositories/MultimediaRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Model.Entities;
+using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Repositories;
+using SweetManagerIotWebService.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using SweetManagerIotWebService.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace SweetManagerIotWebService.API.OrganizationalManagement.Infrastructure.Persistence.EFC.Repositories
+{
+    public class MultimediaRepository(SweetManagerContext context) : BaseRepository<Multimedia>(context), 
+        IMultimediaRepository
+    {
+        public async Task<IEnumerable<Multimedia>> FindAllDetailsByHotelId(int hotelId)
+        => await Context.Set<Multimedia>().Where(m => m.HotelId.Equals(hotelId)).ToListAsync();
+
+
+        public async Task<Multimedia?> FindMainByHotelId(int hotelId)
+        => await Context.Set<Multimedia>().Where(m => m.HotelId.Equals(hotelId)).FirstOrDefaultAsync();
+    }
+}
