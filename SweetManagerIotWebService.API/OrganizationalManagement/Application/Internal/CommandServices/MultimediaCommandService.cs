7af7bfeb6a46e9be8e0bd95491f2cceb110914d2
Mauricio <mauriciorojasanchez@gmail.com>
2025-05-27 00:16:39 -0500

    feat: added command and query services,

---
+++ SweetManagerIotWebService.API/OrganizationalManagement/Application/Internal/CommandServices/MultimediaCommandService.cs
@@ -0,0 +1,36 @@
+﻿using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Model.Commands;
+using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Model.Entities;
+using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Repositories;
+using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Services;
+using SweetManagerIotWebService.API.Shared.Domain.Repositories;
+
+namespace SweetManagerIotWebService.API.OrganizationalManagement.Application.Internal.CommandServices
+{
+    public class MultimediaCommandService(IMultimediaRepository multimediaRepository,
+        IUnitOfWork unitOfWork) : IMultimediaCommandService
+    {
+        public async Task<Multimedia?> Handle(CreateMultimediaCommand command)
+        {
+            var entity = new Multimedia(command);
+
+            await multimediaRepository.AddAsync(entity);
+
+            await unitOfWork.CommitAsync();
+
+            return entity;
+        }
+
+        public async Task<Multimedia?> Handle(UpdateMultimediaCommand command)
+        {
+            var entity = await multimediaRepository.FindByIdAsync(command.Id) ?? throw new Exception("The given ID is not valid.");
+            
+            entity!.UpdateData(command);
+
+            multimediaRepository.Update(entity);
+
+            await unitOfWork.CommitAsync();
+
+            return entity;
+        }
+    }
+}
