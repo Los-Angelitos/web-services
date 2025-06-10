08d0ba3ea0015fdff2e7a2be41028318a369b509
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-09 21:10:29 -0500

    feat: added mail controller.

---
+++ SweetManagerIotWebService.API/Communication/Interfaces/REST/MailController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SweetManagerIotWebService.API.IAM.Infrastructure.Pipeline.Middleware.Attributes;
+using System.Net.Mime;
+
+namespace SweetManagerIotWebService.API.Communication.Interfaces.REST
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Authorize]
+    public class MailController : ControllerBase
+    {
+
+    }
+}
