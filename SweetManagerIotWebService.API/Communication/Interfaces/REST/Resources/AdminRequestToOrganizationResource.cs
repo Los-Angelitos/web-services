b0eb30188ded7cfb7301b0e3fff938ad82db7031
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-09 21:10:10 -0500

    feat: added admin request organziation resource.

---
+++ SweetManagerIotWebService.API/Communication/Interfaces/REST/Resources/AdminRequestToOrganizationResource.cs
@@ -0,0 +1,4 @@
+﻿namespace SweetManagerIotWebService.API.Communication.Interfaces.REST.Resources
+{
+    public record AdminRequestToOrganizationResource(int AdminId, string AdditionalMessage, int HotelId);
+}
