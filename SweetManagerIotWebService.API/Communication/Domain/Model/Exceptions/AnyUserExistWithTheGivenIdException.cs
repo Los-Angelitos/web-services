13e98de1986f14768ba31c9e23639c0e6c3d78bc
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-11 01:41:41 -0500

    feat: added exceptions for validations.

---
+++ SweetManagerIotWebService.API/Communication/Domain/Model/Exceptions/AnyUserExistWithTheGivenIdException.cs
@@ -0,0 +1,4 @@
+﻿namespace SweetManagerIotWebService.API.Communication.Domain.Model.Exceptions
+{
+    public class AnyUserExistWithTheGivenIdException() : Exception("Any user exist with the given id.");
+}
