ca4545d416c5c687b14de5007c7cbe00064f90c6
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-11 01:43:46 -0500

    feat(communication): added endpoint for admin request.

--- SweetManagerIotWebService.API/Communication/Interfaces/REST/MailController.cs
+++ SweetManagerIotWebService.API/Communication/Interfaces/REST/MailController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SweetManagerIotWebService.API.Communication.Domain.Services;
+using SweetManagerIotWebService.API.Communication.Interfaces.REST.Resources;
+using SweetManagerIotWebService.API.Communication.Interfaces.REST.Transform;
 using SweetManagerIotWebService.API.IAM.Infrastructure.Pipeline.Middleware.Attributes;
 using System.Net.Mime;
 
@@ -8,8 +11,27 @@ namespace SweetManagerIotWebService.API.Communication.Interfaces.REST
     [Route("api/v1/[controller]")]
     [Produces(MediaTypeNames.Application.Json)]
     [Authorize]
-    public class MailController : ControllerBase
+    public class MailController(INotificationCommandService notificationCommandService) : ControllerBase
     {
+        [HttpPost("admin-request")]
+        public async Task<IActionResult> AdminRequestToOrganization([FromBody] CreateAdminRequestToOrganizationResource resource)
+        {
+            try
+            {
+                var createAdminRequest = CreateAdminRequestCommandFromResourceAssembler.ToCommandFromResource(resource);
 
+                await notificationCommandService.Handle(createAdminRequest);
+
+                return Ok();
+            }
+            catch (InvalidOperationException)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        } 
     }
 }
