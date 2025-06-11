a7c3bc0510cb90d2ea218c674e26f453f929b737
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-11 01:41:12 -0500

    feat: added value objects for incoming resources.

---
+++ SweetManagerIotWebService.API/Communication/Domain/Model/ValueObjects/RecoveredAdmin.cs
@@ -0,0 +1,6 @@
+﻿using SweetManagerIotWebService.API.IAM.Domain.Model.Aggregates;
+
+namespace SweetManagerIotWebService.API.Communication.Domain.Model.ValueObjects
+{
+    public record RecoveredAdmin(string Name, string FullName, string Email, string Phone);
+}
