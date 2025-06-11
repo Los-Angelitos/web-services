0a1dde85bd9dc68126ea04f543c49d56178a1913
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-11 01:44:19 -0500

    feat(iam): added iam context facade.

---
+++ SweetManagerIotWebService.API/IAM/Interfaces/ACL/IIamContextFacade.cs
@@ -0,0 +1,11 @@
+﻿using SweetManagerIotWebService.API.IAM.Domain.Model.Aggregates;
+
+namespace SweetManagerIotWebService.API.IAM.Interfaces.ACL
+{
+    public interface IIamContextFacade
+    {
+        Task<Admin?> FetchAdminByUserId(int id);
+
+        Task<Owner?> FetchOwnerByUserId(int id);
+    }
+}
---
+++ SweetManagerIotWebService.API/IAM/Interfaces/ACL/Services/IamContextFacade.cs
@@ -0,0 +1,38 @@
+﻿using SweetManagerIotWebService.API.IAM.Domain.Model.Aggregates;
+using SweetManagerIotWebService.API.IAM.Domain.Model.Queries.Users;
+using SweetManagerIotWebService.API.IAM.Domain.Services.CommandServices.Users;
+using SweetManagerIotWebService.API.IAM.Domain.Services.QueryServices.Users;
+
+namespace SweetManagerIotWebService.API.IAM.Interfaces.ACL.Services
+{
+    public class IamContextFacade(IAdminQueryService adminQueryService, IOwnerQueryService ownerQueryService) : IIamContextFacade
+    {
+        public async Task<Admin?> FetchAdminByUserId(int id)
+        {
+            try
+            {
+                var admin = await adminQueryService.Handle(new GetUserByIdQuery(id));
+
+                return admin;
+            }
+            catch(Exception)
+            {
+                return null;
+            }
+        }
+
+        public async Task<Owner?> FetchOwnerByUserId(int id)
+        {
+            try
+            {
+                var owner = await ownerQueryService.Handle(new GetUserByIdQuery(id));
+
+                return owner;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
