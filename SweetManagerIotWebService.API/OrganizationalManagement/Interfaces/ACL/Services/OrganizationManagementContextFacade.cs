3bec5e0788d50744274ecb115f2e0cfe8ff6ef82
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-11 01:45:40 -0500

    feat(organization): added context facade.

---
+++ SweetManagerIotWebService.API/OrganizationalManagement/Interfaces/ACL/Services/OrganizationManagementContextFacade.cs
@@ -0,0 +1,25 @@
+﻿using SweetManagerIotWebService.API.OrganizationalManagement.Application.Internal.OutboundServices.ACL;
+using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Model.Queries;
+using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Services;
+
+namespace SweetManagerIotWebService.API.OrganizationalManagement.Interfaces.ACL.Services
+{
+    public class OrganizationManagementContextFacade(IHotelQueryService hotelQueryService, ExternalIamService externalIamService) : IOrganizationManagementContextFacade
+    {
+        public async Task<int> FetchOwnerIdByHotelId(int hotelId)
+        {
+            try
+            {
+                var hotel = await hotelQueryService.Handle(new GetHotelByIdQuery(hotelId));
+
+                if (hotel is null) return 0;
+
+                return hotel.OwnerId;
+            }
+            catch(Exception)
+            {
+                return 0;
+            }
+        }
+    }
+}
