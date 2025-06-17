a7a3de55d6bb72423fdf38c290ba76a019692f73
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-16 23:52:50 -0500

    feat(fog): added transformers.

---
+++ SweetManagerIotWebService.API/OrganizationalManagement/Interfaces/REST/Transform/CreateFogServerCommandFromResourceAssembler.cs
@@ -0,0 +1,13 @@
+﻿using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Model.Commands;
+using SweetManagerIotWebService.API.OrganizationalManagement.Interfaces.REST.Resources;
+
+namespace SweetManagerIotWebService.API.OrganizationalManagement.Interfaces.REST.Transform
+{
+    public static class CreateFogServerCommandFromResourceAssembler
+    {
+        public static CreateFogServerCommand ToCommandFromResource(CreateFogServerResource resource)
+        {
+            return new CreateFogServerCommand(resource.IpAddress, resource.SubnetMask, resource.HotelId);
+        }
+    }
+}
