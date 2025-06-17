bee714c28cb93e3cdba021683dbb33220c867646
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-16 23:51:04 -0500

    feat(fog): added repository and services.

---
+++ SweetManagerIotWebService.API/OrganizationalManagement/Domain/Services/IFogServerCommandService.cs
@@ -0,0 +1,13 @@
+﻿using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Model.Commands;
+using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Model.Entities;
+
+namespace SweetManagerIotWebService.API.OrganizationalManagement.Domain.Services
+{
+    public interface IFogServerCommandService
+    {
+        Task<FogServer?> Handle(CreateFogServerCommand command);
+
+        Task<FogServer?> Handle(UpdateFogServerCommand command);
+
+    }
+}
