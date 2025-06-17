fee021f1e146a6adb623c3aa9bed2ca8e33fe896
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-16 23:52:33 -0500

    feat(fog): added resources.

---
+++ SweetManagerIotWebService.API/OrganizationalManagement/Interfaces/REST/Resources/FogServerResource.cs
@@ -0,0 +1,4 @@
+﻿namespace SweetManagerIotWebService.API.OrganizationalManagement.Interfaces.REST.Resources
+{
+    public record FogServerResource(int Id, string IpAddress, string SubnetMask, int HotelId);
+}
