9934816eb42985aad68f09612ef4a50ad1fd611c
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-16 23:52:16 -0500

    feat(fog): added command and query service implementation.

---
+++ SweetManagerIotWebService.API/OrganizationalManagement/Application/Internal/QueryServices/FogServerQueryService.cs
@@ -0,0 +1,17 @@
+﻿using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Model.Entities;
+using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Model.Queries;
+using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Repositories;
+using SweetManagerIotWebService.API.OrganizationalManagement.Domain.Services;
+
+namespace SweetManagerIotWebService.API.OrganizationalManagement.Application.Internal.QueryServices
+{
+    public class FogServerQueryService(IFogServerRepository fogServerRepository) : IFogServerQueryService
+    {
+        public async Task<FogServer?> Handle(GetFogServerByHotelIdQuery query)
+         => await fogServerRepository.FindByHotelIdAsync(query.HotelId);
+
+        public async Task<FogServer?> Handle(GetFogServerByIdQuery query)
+         => await fogServerRepository.FindByIdAsync(query.Id);
+
+    }
+}
