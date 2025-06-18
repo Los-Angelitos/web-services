ea92bbd3654edcccd00f65de7b9cf88c6a46cbb7
Salvlormonch1 <salvlormonch@gmail.com>
2025-06-17 20:59:56 -0500

    feat(rfid): added RFID transform files.

---
+++ SweetManagerIotWebService.API/Inventory/Interfaces/REST/Transform/RfidCardResourceFromEntityAssembler.cs
@@ -0,0 +1,16 @@
+﻿using SweetManagerIotWebService.API.Inventory.Domain.Model.Aggregates;
+using SweetManagerIotWebService.API.Inventory.Interfaces.REST.Resources;
+
+namespace SweetManagerIotWebService.API.Inventory.Interfaces.REST.Transform;
+
+public static class RfidCardResourceFromEntityAssembler
+{
+    public static RfidCardResource ToResourceFromEntity(RfidCard rfidCard)
+    {
+        return new RfidCardResource(
+            rfidCard.Id,
+            rfidCard.RoomId,
+            rfidCard.apiKey,
+            rfidCard.uId);
+    }
+}
