de74c78e48072d4a3bb37b8a6ce4abdc6410a3ff
Salvlormonch1 <salvlormonch@gmail.com>
2025-06-17 20:51:35 -0500

    feat(rfid): added RFID Card command and query services.

---
+++ SweetManagerIotWebService.API/Inventory/Application/Internal/QueryServices/RfidCardQueryService.cs
@@ -0,0 +1,14 @@
+﻿using SweetManagerIotWebService.API.Inventory.Domain.Model.Aggregates;
+using SweetManagerIotWebService.API.Inventory.Domain.Model.Queries.RFID;
+using SweetManagerIotWebService.API.Inventory.Domain.Repositories;
+using SweetManagerIotWebService.API.Inventory.Domain.Services;
+
+namespace SweetManagerIotWebService.API.Inventory.Application.Internal.QueryServices;
+
+public class RfidCardQueryService(IRfidCardRepository rfidCardRepository) : IRfidCardQueryService
+{
+    public async Task<IEnumerable<RfidCard>> Handle(GetAllRfidCardsQuery query)
+    {
+        return await rfidCardRepository.ListAsync();
+    }
+}
